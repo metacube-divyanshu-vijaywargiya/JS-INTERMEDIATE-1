df72f48f5e342a5f607dd958007ac0bc5c8788d3
Divyanshu Vijaywargiya <divyanshu.vijaywargiya@metacube.com>
2025-01-22 15:50:23 +0530

    second assignment added

--- JS-INTERMEDIATE-2.js
+++ JS-INTERMEDIATE-2.js
@@ -1,17 +1,21 @@
-//Method to find fibonacci number 
-function fibonacci(n){
-    if(n==0){
-        return 0;
+//method to find array of n fibonacci numbers
+function fibonacciArrayMethod(n){
+    const fibonacciArray = [];
+    
+    for (let i = 0; i < n; i++) {
+        if (i === 0) {
+            fibonacciArray.push(0);
+        } else if (i === 1) {
+            fibonacciArray.push(1);
+        } else {
+            fibonacciArray.push(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
+        }
     }
     
-    if(n == 1){
-        return 1;
-    }
-    
-    return fibonacci(n-1)+ fibonacci(n-2);
+    return fibonacciArray;
 }
 
-//printing the function result
-console.log(fibonacci(6)); //output 8
-console.log(fibonacci(5)); //output 5
-console.log(fibonacci(16)); //output 987
+//printing results array
+console.log(fibonacciArrayMethod(6)); //output [0, 1, 1, 2, 3, 5 ]
+console.log(fibonacciArrayMethod(5)); //output [0, 1, 1, 2, 3 ]
+console.log(fibonacciArrayMethod(16)); //output [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
