65b036d4601ebbb1830f1bebc1d5ef395cf5a370
Divyanshu Vijaywargiya <divyanshu.vijaywargiya@metacube.com>
2025-02-04 10:32:52 +0530

    First assignment updated

--- JS-INTERMEDIATE-1.js
+++ JS-INTERMEDIATE-1.js
@@ -3,30 +3,42 @@ function findSecondLowestAndGreatest(arr) {
         return "Array must contain atleast two numbers.";
     }
 
-    let lowest = Infinity;
-    let secondLowest = Infinity;
-    let greatest = -Infinity;
-    let secondGreatest = -Infinity;
+    // APPROACH - 1
+    // let lowest = Infinity;
+    // let secondLowest = Infinity;
+    // let greatest = -Infinity;
+    // let secondGreatest = -Infinity;
 
-    for (let i = 0 ; i < arr.length ; i++) {
-        // Update lowest and second lowest
-        if (arr[i] < lowest) {
-            secondLowest = lowest;
-            lowest = arr[i];
-        } else if (arr[i] < secondLowest && arr[i] !== lowest) {
-            secondLowest = arr[i];
-        }
+    // for (let i = 0 ; i < arr.length ; i++) {
+    //     // Update lowest and second lowest
+    //     if (arr[i] < lowest) {
+    //         secondLowest = lowest;
+    //         lowest = arr[i];
+    //     } else if (arr[i] < secondLowest && arr[i] !== lowest) {
+    //         secondLowest = arr[i];
+    //     }
 
-        // Update greatest and second greatest
-        if (arr[i] > greatest) {
-            secondGreatest = greatest;
-            greatest = arr[i];
-        } else if (arr[i] > secondGreatest && arr[i] !== greatest) {
-            secondGreatest = arr[i];
-        }
-    }
+    //     // Update greatest and second greatest
+    //     if (arr[i] > greatest) {
+    //         secondGreatest = greatest;
+    //         greatest = arr[i];
+    //     } else if (arr[i] > secondGreatest && arr[i] !== greatest) {
+    //         secondGreatest = arr[i];
+    //     }
+    // }
 
-    return [secondLowest, secondGreatest];
+    // return [secondLowest, secondGreatest];
+
+
+    // APPROACH - 2
+    // Use Set to remove duplicate numbers and then convert it to array
+    const uniqueValues = Array.from(new Set(arr));
+
+    // Sort the array
+    uniqueValues.sort((a, b) => a - b);
+
+    // Return the second lowest and second greatest
+    return [uniqueValues[1], uniqueValues[uniqueValues.length - 2]];
 }
 
 // Input sample array
